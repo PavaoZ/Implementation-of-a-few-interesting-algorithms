b9ced1be575751b8381556b7c4c1f1ded1769aa2
Pavo <pzornija1@etf.unsa.ba>
2021-01-02 00:30:01 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Implementation_of_a_few_interesting_algorithms.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Implementation_of_a_few_interesting_algorithms.dir/main.cpp.o"
+  "Implementation_of_a_few_interesting_algorithms.exe"
+  "Implementation_of_a_few_interesting_algorithms.pdb"
+  "libImplementation_of_a_few_interesting_algorithms.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Implementation_of_a_few_interesting_algorithms.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
