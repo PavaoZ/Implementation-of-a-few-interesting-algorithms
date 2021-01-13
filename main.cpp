a6366393cc4c63f044b75ab73c63d98d4cf44069
Pavo <pzornija1@etf.unsa.ba>
2021-01-13 01:05:57 +0100

    Finished first task for LV5

--- main.cpp
+++ main.cpp
@@ -10,10 +10,12 @@ class Formula3CNF {
 private:
     vector<vector<int>> logic_formula_clauses;
     vector<vector<int>> logic_formula_table_of_combinations;
-    int n = 0, maxClausesNumber = 8, maxLogicVariables = 24;
+    int n = 0, maxClausesNumber = 8, maxLogicVariables = 24, numberOfLogicVariables = 0;
 
-    bool checkIfTooManyLogicVariables() {
-
+    bool checkIfTooManyLogicVariables(vector<vector<int>> checkInsideVector, int maxVar) {
+        for(int i = 0; i < checkInsideVector.size(); ++i)
+            this->numberOfLogicVariables += checkInsideVector[i].size();
+        return this->numberOfLogicVariables > maxVar;
     }
 public:
     // unos3CNF function
@@ -22,7 +24,6 @@ public:
         string clauses;
         int index = 0;
         while(getline(cin, clauses) && (index < (this->maxClausesNumber - 1))) {
-
             // Example input is 1,2,3
             // If no next character is added then our input ends
             if(clauses.empty()) break;
@@ -48,6 +49,14 @@ public:
                 // etc...
                 if (stream_to_matrix_helper.peek() == ',')
                     stream_to_matrix_helper.ignore();
+            }
+            // Here we check if we have too many logic variable
+            if(checkIfTooManyLogicVariables(this->logic_formula_clauses, this->maxLogicVariables)) {
+                cout << "Too many logic variables! " << "Up to " << this->maxLogicVariables << "!" << endl;
+                vector<vector<int>> forEmpty;
+                this->logic_formula_clauses = forEmpty;
+                this->numberOfLogicVariables = 0;
+                break;
             }
             // We change our row in matrix into which we input so that
             // 1,2,3 goes into one row
@@ -105,12 +114,13 @@ public:
         return solvable;
     }
 
+    // Help function
     bool checkClauseCombination(vector<int> possibleCombinations) {
-        bool verificationControl = true;
+        bool acceptableCombination = true;
 
         for(int i = 0; i < this->logic_formula_clauses.size(); ++i) {
             bool clauseCheck = false;
-            for(unsigned int j = 0; j < this->logic_formula_clauses[i].size(); ++j) {
+            for(int j = 0; j < this->logic_formula_clauses[i].size(); ++j) {
                 // because we input values for the formula starting from 1, we won't really have a case where the input is [0,1,2] for example but always or similar to [1,2,3]
                 // possibleCombinations contains for example [X1 -> 0, X2 -> 0, X3 -> 1], but the indexes in possibleCombinations are from 0 so that why we take the absolute vaue - 1
                 const int index = abs(this->logic_formula_clauses[i][j]) - 1;
@@ -123,18 +133,32 @@ public:
                 }
             }
 
+            // Comment from StackOverFlow
+            // In short, you should go with whichever version is the easiest to read and maintain.
+            // In slightly older times, I know breaking out of a loop was considered to be a no-no (on par with a goto statement).
+            // Loops were supposed to break on the loop condition and nowhere else. Thus, the while-loop would have been the way to go.
+            // (This is probably a holdover from assembly, where loops are basically a block of code with a go-to-the-beginning-if-true jump statement at the end.
+            // Multiple conditional-jump statements in the block make it exceedingly hard to debug; thus they were to be avoided and combined into one at the end.)
+            // Maybe better practice to set j to this->logic_formula_clauses.size() but doesn't really matter
             if(!clauseCheck) {
-                verificationControl = false;
+                acceptableCombination = false;
                 break;
             }
         }
 
-        return verificationControl;
+        return acceptableCombination;
+    }
+
+    // verifikacija3CNF
+    bool verification3CNF(std::vector<int> possibleCombinations) {
+        return checkClauseCombination(possibleCombinations);
     }
 };
 
 int main() {
     Formula3CNF initial;
+    int helpVar;
+    vector<int> helpVec;
 
     cout << "Dobro došli!" << endl;
     cout << "Odaberite jednu od ponuđenih opcija unosom rednog broja!" << endl;
@@ -169,6 +193,7 @@ int main() {
             case 2:
                 break;
             case 3:
+                cout << "Odabrali ste provjeru ispunjivosti formule!\n\n";
                 cin.clear();
                 cin.ignore();
                 if(initial.solvable3CNF())
@@ -181,6 +206,12 @@ int main() {
             case 5:
                 break;
             case 6:
+                cout << "Odabrali ste verifikaciju nekog pridruživanja!\n\n";
+                while(helpVar != -1) {
+                    cin >> helpVar;
+                    if(helpVar == -1) break;
+                    helpVec.push_back(helpVar);
+                }
                 break;
             case 7:
                 break;
