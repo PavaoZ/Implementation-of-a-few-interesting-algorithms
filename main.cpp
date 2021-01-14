0b2d8147f85edb1641126f4590c498002e58e27e
Pavo <pzornija1@etf.unsa.ba>
2021-01-14 04:04:20 +0100

    Some test cases written

--- main.cpp
+++ main.cpp
@@ -18,6 +18,12 @@ struct Node
 
 typedef Node *NodePointer;
 
+NodePointer createNode(int value) {
+    NodePointer x = new Node;
+    x->key = value;
+    return x;
+}
+
 class FIBHeap {
 private:
     int numberOfNodes; // Number of nodes
@@ -25,19 +31,27 @@ private:
 
     NodePointer MakeFIBHeap() {
         this->numberOfNodes = 0;
-        return nullptr;
+//        return nullptr;
         // I think we don't need this at all
-        // NodePointer min_node_pointer = nullptr;
-        // return min_node_pointer;
+         NodePointer min_node_pointer = nullptr;
+         return min_node_pointer;
     }
 public:
     FIBHeap() {
         this->H = MakeFIBHeap();
     }
 
-    int fibHeapLink(NodePointer H1, NodePointer y, NodePointer z) {
-        y->left->right = y->right;
-        y->right->left = y->left;
+    NodePointer getH() {
+        return this->H;
+    }
+
+    int nodeNumber() {
+        return this->numberOfNodes;
+    }
+
+    void fibHeapLink(NodePointer H1, NodePointer y, NodePointer z) {
+        (y->left)->right = y->right;
+        (y->right)->left = y->left;
         if (z->right == z)
             H1 = z;
         y->left = y;
@@ -46,128 +60,118 @@ public:
         if (z->child == nullptr)
             z->child = y;
         y->right = z->child;
-        y->left = z->child->left;
-        z->child->left->right = y;
-        z->child->left = y;
-        if (y->key < z->child->key)
+        y->left = (z->child)->left;
+        ((z->child)->left)->right = y;
+        (z->child)->left = y;
+        if (y->key < (z->child)->key)
             z->child = y;
         z->degree++;
     }
 
-    NodePointer Create_Node(int value) {
-        NodePointer x = new Node;
-        x->key = value;
-        return x;
-    }
-
     // I think we can change this a bit to:
-    NodePointer FIBHeapInsert(NodePointer x) {
+    NodePointer FIBHeapInsert(NodePointer H, NodePointer x) {
         x->degree = 0;
         x->parent = nullptr;
         x->child = nullptr;
+        x->left = x;
+        x->right = x;
         x->mark = 'F';
-        if(this->H == nullptr)
-            this->H = x;
-        else {
-            // This represents adding the element to the list of roots and becomes the left sibling of the root
-            H->left->right = x;
+        x->C = 'N';
+        if (H != nullptr) {
+            (H->left)->right = x;
             x->right = H;
             x->left = H->left;
             H->left = x;
-            if(x->key < this->H->key) this->H = x;
+            if (x->key < H->key)
+                H = x;
         }
+        else H = x;
         this->numberOfNodes++;
-    }
-//    NodePointer FIBHeapInsert(NodePointer H, NodePointer x) {
-//        x->degree = 0;
-//        x->parent = nullptr;
-//        x->child = nullptr;
-//        x->left = x;
-//        x->right = x;
-//        x->mark = 'F';
-//        x->C = 'N';
-//        if (H != nullptr) {
-//            H->left->right = x;
-//            x->right = H;
-//            x->left = H->left;
-//            H->left = x;
-//            if (x->key < H->key)
-//                H = x;
-//        }
-//        else
-//        {
-//            H = x;
-//        }
-//        numberOfNodes = numberOfNodes + 1;
-//        return H;
-//    }
-
-    NodePointer FIBHeapUnion(NodePointer H1, NodePointer H2) {
-        NodePointer temp;
-        NodePointer H = MakeFIBHeap();
-        // We essentially create a new Fibonacci heap that is a mix between the two heaps and essentially a new root list
-        // These are the minimum nodes, or better said the roots
-        H = H1;
-        H->left->right = H2;
-        H2->left->right = H;
-        temp = H->left;
-        H->left = H2->left;
-        H2->left = temp;
         return H;
     }
 
+    NodePointer FIBHeapUnion(NodePointer H1, NodePointer H2) {
+        if(H1 == nullptr && H2 == nullptr)
+            return nullptr;
+        NodePointer temp;
+        // We essentially create a new Fibonacci heap that is a mix between the two heaps and essentially a new root list
+        // These are the minimum nodes, or better said the roots
+        NodePointer H = H1;
+        if(H == nullptr || (H2 != nullptr && (H2->key < H1->key)))
+            H = H2;
+        else {
+            (H->left)->right = H2;
+            (H2->left)->right = H;
+            temp = H->left;
+            H->left = H2->left;
+            H2->left = temp;
+        }
+
+        this->numberOfNodes = getNumberOfNodesFromRootNode(H1) + getNumberOfNodesFromRootNode(H2);
+        cout << this->numberOfNodes << "broj nodova" << endl;
+        return H;
+    }
+
+    int getNumberOfNodesFromRootNode(NodePointer H_min) {
+        if(H_min == nullptr)
+            return 0;
+        NodePointer copy = H_min;
+        int numberOf = 0;
+        do {
+            copy = copy->right;
+            numberOf++;
+        } while (copy != H_min && copy->right != nullptr);
+        return numberOf;
+    }
+
     NodePointer FIBHeapExtractMin(NodePointer H_min) {
-        // H1 is the heap we are extracting from
-        NodePointer p, x = nullptr, temp2, temp, z = H_min;
+        // H_min is the heap we are extracting from
+        NodePointer p, tempPtr, z = H_min;
         p = z;
-        temp2 = z;
-        if (z == nullptr) return z;
-
-        // We set the initial value of x and we don't need this
-        // x = nullptr;
-        // We initialize the first child because we need to go through all the children x of z, with a check of nullptr
-        if (z->child != nullptr) x = z->child;
-
-        if(x != nullptr) {
-            temp2 = x;
+        tempPtr = z;
+        if (z == nullptr)
+            return z;
+        NodePointer x, tempPtr2;
+        x = nullptr;
+        if (z->child != nullptr)
+            x = z->child;
+        if (x != nullptr){
+            tempPtr = x;
             do {
-                temp = x->right;
-                H_min->left->right = x;
+                tempPtr2 = x->right;
+                (H_min->left)->right = x;
                 x->right = H_min;
                 x->left = H_min->left;
                 H_min->left = x;
                 if (x->key < H_min->key)
                     H_min = x;
                 x->parent = nullptr;
-                x = temp;
-            } while (temp != temp2);
+                x = tempPtr2;
+            } while (tempPtr2 != tempPtr);
         }
-
-        z->left->right = z->right;
-        z->right->left = z->left;
-        H_min = z->right;
+        (z->left)->right = z->right;
+        (z->right)->left = z->left;
+//        H_min = z->right;
         if (z == z->right && z->child == nullptr)
-            this->H = nullptr;
+            H = nullptr;
         else {
             H_min = z->right;
             Consolidate(H_min);
         }
-
-        this->numberOfNodes--;
+        this->H = this->H - 1;
         return p;
     }
 
-    int Consolidate(NodePointer H_min) {
+    void Consolidate(NodePointer H_min) {
         int d, i;
         float f = (log(this->numberOfNodes)) / (log(2));
         int D = f;
         NodePointer A[D];
         for (i = 0; i <= D; i++) A[i] = nullptr;
-        NodePointer x = H_min;
-        NodePointer y, temp;
-        NodePointer pt = x;
+        NodePointer x = H_min, y, temp;
+        NodePointer temp2 = x;
         do {
-            pt = pt->right;
+            temp2 = temp2->right;
             d = x->degree;
             while (A[d] != nullptr) {
                 y = A[d];
@@ -179,8 +183,9 @@ public:
 
                 if (y == H_min) H_min = x;
                 fibHeapLink(H_min, y, x);
-                if (x->right == x) H_min = x;
-                A[d] = nullptr;
+                if (x->right == x)
+                    H_min = x;
+                    A[d] = nullptr;
                 d++;
             }
             A[d] = x;
@@ -208,93 +213,98 @@ public:
         }
     }
 
-    int Display(NodePointer H) {
-        NodePointer p = H;
-        if (p == NULL)
-        {
-            cout<<"The Heap is Empty"<<endl;
+    int FIBHeapDecreaseKey(NodePointer H1, int x, int k) {
+        NodePointer y;
+        if (H1 == nullptr) {
+            cout << "Heap empty" << endl;
             return 0;
         }
-        cout<<"The root Nodes of Heap are: "<<endl;
-        do
-        {
-            cout<<p->key;
-            p = p->right;
-            if (p != H)
-            {
-                cout<<"-->";
-            }
+
+        // We find the node with the key
+        NodePointer tempPtr = FindNode(H1, x);
+
+        if (tempPtr == nullptr) {
+            cout << "Node not found in the Heap" << endl;
+            return 1;
         }
-        while (p != H && p->right != NULL);
-        cout<<endl;
+
+        if (tempPtr->key < k) {
+            cout << "New key is greater than current key" << endl;
+            return 0;
+        }
+
+        tempPtr->key = k;
+        y = tempPtr->parent;
+
+        if (y != nullptr && tempPtr->key < y->key) {
+            Cut(H1, tempPtr, y);
+            Cascading_cut(H1, y);
+        }
+
+        if (tempPtr->key < H->key)
+            this->H = tempPtr;
+        return 0;
     }
 
-    NodePointer Find(NodePointer H, int k) {
+
+    void Display(NodePointer H) {
+        NodePointer p = H;
+        if (p == nullptr) {
+            cout << "The Heap is Empty" << endl;
+            return;
+        }
+
+        cout << "The root Nodes of Heap are: " << endl;
+
+        do {
+            cout << p->key;
+            p = p->right;
+            if (p != H)
+                cout << "-->";
+        } while (p != H && p->right != nullptr);
+
+        cout << endl;
+    }
+
+    NodePointer FindNode(NodePointer H, int k) {
         NodePointer x = H;
         x->C = 'Y';
-        NodePointer p = NULL;
-        if (x->key == k)
-        {
+        NodePointer p = nullptr;
+
+        if (x->key == k) {
             p = x;
             x->C = 'N';
             return p;
         }
-        if (p == NULL)
-        {
-            if (x->child != NULL )
-                p = Find(x->child, k);
+
+        if (p == nullptr) {
+            if (x->child != nullptr )
+                p = FindNode(x->child, k);
             if ((x->right)->C != 'Y' )
-                p = Find(x->right, k);
+                p = FindNode(x->right, k);
         }
+
         x->C = 'N';
+
         return p;
     }
 
-    int FIBHeapDecreaseKey(NodePointer H1, int x, int k) {
-        NodePointer y;
-        if (H1 == NULL)
-        {
-            cout<<"The Heap is Empty"<<endl;
-            return 0;
-        }
-        NodePointer ptr = Find(H1, x);
-        if (ptr == NULL)
-        {
-            cout<<"Node not found in the Heap"<<endl;
-            return 1;
-        }
-        if (ptr->key < k)
-        {
-            cout<<"Entered key greater than current key"<<endl;
-            return 0;
-        }
-        ptr->key = k;
-        y = ptr->parent;
-        if (y != NULL && ptr->key < y->key)
-        {
-            Cut(H1, ptr, y);
-            Cascading_cut(H1, y);
-        }
-        if (ptr->key < H->key)
-            H = ptr;
-        return 0;
-    }
     int FIBHeapDelete(NodePointer H1, int k) {
-        NodePointer np = NULL;
-        int t;
-        t = FIBHeapDecreaseKey(H1, k, -5000);
-        if (!t)
-            np = FIBHeapExtractMin(H);
-        if (np != NULL)
-            cout<<"Key Deleted"<<endl;
+        NodePointer tempPtr = nullptr;
+        int rez = FIBHeapDecreaseKey(H1, k, -5000);
+
+        if (!rez)
+            tempPtr = FIBHeapExtractMin(this->H);
+        if (tempPtr != nullptr)
+            cout << "Key Deleted" << endl;
         else
-            cout<<"Key not Deleted"<<endl;
+            cout << "Key not Deleted" << endl;
         return 0;
     }
 
-    int Cut(NodePointer H1, NodePointer x, NodePointer y) {
+    void Cut(NodePointer H1, NodePointer x, NodePointer y) {
         if (x == x->right)
-            y->child = NULL;
+            y->child = nullptr;
         (x->left)->right = x->right;
         (x->right)->left = x->left;
         if (x == y->child)
@@ -306,30 +316,60 @@ public:
         x->right = H1;
         x->left = H1->left;
         H1->left = x;
-        x->parent = NULL;
+        x->parent = nullptr;
         x->mark = 'F';
     }
-    int Cascading_cut(NodePointer H1, NodePointer y) {
+
+    void Cascading_cut(NodePointer H1, NodePointer y) {
         NodePointer z = y->parent;
-        if (z != NULL)
-        {
+        if (z != nullptr)
             if (y->mark == 'F')
-            {
                 y->mark = 'T';
-            }
-            else
-            {
+            else {
                 Cut(H1, y, z);
                 Cascading_cut(H1, z);
             }
-        }
     }
 };
 
 int main()
 {
+    FIBHeap initial;
+    FIBHeap initial2;
+    NodePointer H, H2;
+    H = initial.getH();
+    H2 = initial2.getH();
     cout << "Welcome to the Fibonacci heap test cases!" << endl;
-    // Test cases
+    cout << "Next we will have a bunch of called test cases!" << endl;
+    // TEST CASES:
+
+    // 1. INSERT TEST CASES
+    // 5 nodes inputted
+    // Situation when we create a root list with just x
+    H = initial.FIBHeapInsert(H, createNode(1));
+    // Situation when we just add new nodes to heap
+    H = initial.FIBHeapInsert(H, createNode(2));
+    H = initial.FIBHeapInsert(H, createNode(23));
+    H = initial.FIBHeapInsert(H, createNode(15));
+    // Situation when the root node changes
+    H = initial.FIBHeapInsert(H, createNode(-15));
+
+    // 2. UNION TEST CASES
+    // Additional heap that is created
+    H2 = initial2.FIBHeapInsert(H2, createNode(1));
+    H2 = initial2.FIBHeapInsert(H2, createNode(2));
+    H2 = initial2.FIBHeapInsert(H2, createNode(23));
+    H2 = initial2.FIBHeapInsert(H2, createNode(15));
+    // Union operation
+    // Special operation when we call it one after the other
+    H = initial.FIBHeapUnion(nullptr, H2);
+    H = initial.FIBHeapUnion(nullptr, nullptr);
+    H = initial.FIBHeapUnion(H, H2);
+    H2 = initial2.FIBHeapUnion(H, H2);
+
+
+    initial.Display(H);
+    initial2.Display(H2);
 
     return 0;
 }
